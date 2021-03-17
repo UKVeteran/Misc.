52f94b38e91b0af146a9f85c608a2c13212b7289
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2021-03-17 23:01:03 +0000

    Add files via upload

---
+++ textmanipulation.py
@@ -0,0 +1,35 @@
+'this is my string'.split()
+
+s = ' this   is  my string '
+
+s.split()
+
+ s.split(' ')
+ 
+ s.split(maxsplit=1)
+ 
+ 'a' + 'b' + 'c'
+'abc'
+
+'do' * 2
+'dodo'
+
+strings = ['do', 're', 'mi']
+','.join(strings)
+
+
+s1 = 'Apple'
+s2 = 'Pie'
+s3 = 'Sauce'
+
+s4 = s1 + s2 + s3
+
+print(s4)
+
+
+s1 = input('Please enter the first string:\n')
+s2 = input('Please enter the second string:\n')
+
+print('Concatenated String =', s1 + s2)
+
+
